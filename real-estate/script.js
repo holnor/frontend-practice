ee7cec899478f7600fdca336d77479ac4b6c5356
holczur <holcz.norbert@gmail.com>
2023-07-12 15:32:37 +0200

    body selected

--- real-estate/script.js
+++ real-estate/script.js
@@ -3,6 +3,7 @@ const nav = document.querySelector(".outer");
 const barTop = document.querySelector(".top");
 const barMiddle = document.querySelector(".mid");
 const barBottom = document.querySelector(".bot");
+const body = document.querySelector("body");
 
 
 menuBtn.addEventListener('click', ()=>{
@@ -10,5 +11,4 @@ menuBtn.addEventListener('click', ()=>{
     barTop.classList.toggle("closed");
     barMiddle.classList.toggle("closed");
     barBottom.classList.toggle("closed");
-
 })
