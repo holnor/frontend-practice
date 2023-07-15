a50490bad4d88647c8fb39994e099f074215183e
holczur <holcz.norbert@gmail.com>
2023-07-15 07:43:02 +0200

    horizontal scroll for special offers about us section

--- real-estate/script.js
+++ real-estate/script.js
@@ -14,26 +14,26 @@ menuBtn.addEventListener('click', ()=> {
 });
 
 
-const header = document.getElementById("header");
-const title = document.getElementById("title");
-const excerpt = document.getElementById("excerpt");
-const profileImg = document.getElementById("profile_img");
-const name = document.getElementById("name");
-const date = document.getElementById("date");
-
-const animatedBgs = document.querySelectorAll('.animated-bg');
-const animatedBgTexts = document.querySelectorAll('.animated-bg-text');
-
-setTimeout(getData, 2500);
-function getData() {
-    header.innerHTML =
-        '<img class="card-img special-offer_img" src="https://images.unsplash.com/photo-1613490493576-7fde63acd811?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1171&q=80" alt="" />'
-    title.innerHTML = 'Lorem ipsum dolor sit amet.';
-    excerpt.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alic ducimus laudantium qui quidem recusandae voluptatem?';
-    profileImg.innerHTML = '<img src="https://xsgames.co/randomusers/avatar.php?g=female" alt=""/>';
-    name.innerHTML = 'Lorrine Soehren';
-    date.innerHTML = 'Jul 14 2023';
-
-    animatedBgs.forEach(bg => bg.classList.remove('animated-bg'));
-    animatedBgTexts.forEach(bgT => bgT.classList.remove('animated-bg-text'));
-}
+// const header = document.getElementById("header");
+// const title = document.getElementById("title");
+// const excerpt = document.getElementById("excerpt");
+// const profileImg = document.getElementById("profile_img");
+// const name = document.getElementById("name");
+// const date = document.getElementById("date");
+//
+// const animatedBgs = document.querySelectorAll('.animated-bg');
+// const animatedBgTexts = document.querySelectorAll('.animated-bg-text');
+//
+// setTimeout(getData, 2500);
+// function getData() {
+//     header.innerHTML =
+//         '<img class="card-img special-offer_img" src="https://images.unsplash.com/photo-1613490493576-7fde63acd811?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1171&q=80" alt="" />'
+//     title.innerHTML = 'Lorem ipsum dolor sit amet.';
+//     excerpt.innerHTML = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alic ducimus laudantium qui quidem recusandae voluptatem?';
+//     profileImg.innerHTML = '<img src="https://xsgames.co/randomusers/avatar.php?g=female" alt=""/>';
+//     name.innerHTML = 'Lorrine Soehren';
+//     date.innerHTML = 'Jul 14 2023';
+//
+//     animatedBgs.forEach(bg => bg.classList.remove('animated-bg'));
+//     animatedBgTexts.forEach(bgT => bgT.classList.remove('animated-bg-text'));
+// }
